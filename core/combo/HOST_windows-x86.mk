a83fb6850bfeac2d70e907993237636c5d4e57b1
Alexander Martinz <eviscerationls@gmail.com>
2014-01-01 18:14:25 +0100

    update build, sync parts with cyanogenmod
    Change-Id: Ife632bb924b98512707d44844f7709ab43853700

--- core/combo/HOST_windows-x86.mk
+++ core/combo/HOST_windows-x86.mk
@@ -29,13 +29,23 @@ HOST_ACP_UNAVAILABLE := true
 TOOLS_EXE_SUFFIX :=
 HOST_GLOBAL_CFLAGS += -DUSE_MINGW
 ifneq ($(strip $(BUILD_HOST_64bit)),)
+ifeq ($(BUILD_OS),darwin)
+$(error Windows amd64 cross compile builds not supported from Darwin)
+else
 TOOLS_PREFIX := /usr/bin/amd64-mingw32msvc-
 HOST_C_INCLUDES += /usr/lib/gcc/amd64-mingw32msvc/4.4.2/include
 HOST_GLOBAL_LD_DIRS += -L/usr/amd64-mingw32msvc/lib
+endif
+else
+ifeq ($(BUILD_OS),darwin)
+TOOLS_PREFIX := /usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc/bin//i586-mingw32-
+HOST_C_INCLUDES += /usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc/i586-mingw32/include /usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc/i586-mingw32/include/ddk
+HOST_GLOBAL_LD_DIRS += -L/usr/local/gcc-4.8.0-qt-4.8.4-for-mingw32/win32-gcc/i586-mingw32/lib
 else
 TOOLS_PREFIX := /usr/bin/i586-mingw32msvc-
 HOST_C_INCLUDES += /usr/lib/gcc/i586-mingw32msvc/3.4.4/include
 HOST_GLOBAL_LD_DIRS += -L/usr/i586-mingw32msvc/lib
+endif
 endif # BUILD_HOST_64bit
 endif # USE_MINGW
 endif # Linux
