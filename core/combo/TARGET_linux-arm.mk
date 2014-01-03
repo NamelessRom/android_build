0799ca42c913d2dbdcba004a79d3cb82e4707e10
Alexander Martinz <eviscerationls@gmail.com>
2014-01-03 16:18:48 +0100

    ifdef added customizations more
    Change-Id: Ie91b781e2d7697b84f240771b6065f994de51a62
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -93,7 +93,9 @@ ifeq ($(TARGET_USE_O3),true)
                         -pipe
 else
     TARGET_thumb_CFLAGS :=  -mthumb \
-                            -Os \
+                            -Os
+ifeq ($(TARGET_OPTIMIZE_NO_O3),true)
+    TARGET_thumb_CFLAGS +=
                             -fomit-frame-pointer \
                             -fstrict-aliasing \
                             -funsafe-math-optimizations \
@@ -101,6 +103,7 @@ else
                             -fno-strict-aliasing \
                             -funroll-loops \
                             -pipe
+endif
 endif
 
 TARGET_arm_CFLAGS +=    -Wno-unused-parameter \
