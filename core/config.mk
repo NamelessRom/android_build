dc0d2652fcbe654c4485c01ecdaa0091d356787b
jumoog <kilianvonpflugk@gmail.com>
2014-02-28 00:38:44 +0100

    Fix global selinux policy inclusion
    Change-Id: I389177c1b6518e7a64c0c8409a1695a1669d5ee5

--- core/config.mk
+++ core/config.mk
@@ -506,10 +506,10 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
-#ifneq ($(CUSTOM_BUILD),)
+ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-#$(eval include vendor/nameless/sepolicy/sepolicy.mk)
-#endif
+$(eval include vendor/nameless/sepolicy/sepolicy.mk)
+endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
