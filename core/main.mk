86f7f21b5e4834d6d78b68225392ecdadd579a4e
Evisceration <eviscerationls@gmail.com>
2014-04-02 17:17:32 +0200

    do not stop on wrong java version, instead throw a warning
    Change-Id: I9120355dfe385e8d483f175ddcfb36c61be50a47

--- core/main.mk
+++ core/main.mk
@@ -185,7 +185,7 @@ $(info $(space))
 $(info Please follow the machine setup instructions at)
 $(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
-$(error stop)
+$(info You may continue but you are warned!)
 endif
 
 ifndef BUILD_EMULATOR
