c03b761b0fc59e99b226f79a2a96ecda9a391930
Evisceration <eviscerationls@gmail.com>
2014-07-15 01:43:09 +0200

    add nhw to boot jars
    Change-Id: I14725753f1ae6a0a4c5f470888d4ddb9b18f927d

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -64,8 +64,9 @@ PRODUCT_PACKAGES += \
     screenrecord \
     telephony-common \
     voip-common \
-    telephony-msim
+    telephony-msim \
+    org.namelessrom.hardware
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 # Override the PRODUCT_BOOT_JARS set in core_minimal.mk
-PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium:telephony-msim
+PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium:telephony-msim:org.namelessrom.hardware
