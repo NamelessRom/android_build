93741c07b543d363d9ddfa820b6a0be0eae446a7
arter97 <qkrwngud825@gmail.com>
2014-09-09 11:44:22 +0900

    Apply correct, optimized mfpu compiler flag for ARMv7-A Cortex CPUs
    For those ARMv7-A Cortex CPUs that can handle VFPv4 floating point,
    We can set "-mfpu=neon-vfpv4" instead of generic "-mfpu=neon"
    to gain extra performance improvements.
    
    References :
     - GCC		: https://gcc.gnu.org/onlinedocs/gcc/ARM-Options.html
     - Cortex A15	: http://www.arm.com/products/processors/cortex-a/cortex-a15.php
     - Cortex A9	: http://www.arm.com/products/processors/cortex-a/cortex-a9.php
     - Cortex A8	: http://www.arm.com/products/processors/cortex-a/cortex-a8.php
     - Cortex A7	: http://www.arm.com/products/processors/cortex-a/cortex-a7.php
     - Cortex A5	: http://www.arm.com/products/processors/cortex-a/cortex-a5.php
    
    Change-Id: Id8b1a251234014ecf7ffbc1ae14455ed1726e028
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,25 +7,25 @@ ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
 ifeq ($(TARGET_CPU_VARIANT),$(filter $(TARGET_CPU_VARIANT),cortex-a15 krait))
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags := -mcpu=cortex-a15 -mfpu=neon-vfpv4
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9
+	arch_variant_cflags := -mcpu=cortex-a9 -mfpu=neon
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags  := -mcpu=cortex-a8 -mfpu=neon
 	arch_variant_ldflags := -Wl,--fix-cortex-a8
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
+	arch_variant_cflags := -mcpu=cortex-a7 -mfpu=neon-vfpv4
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a5)
-	arch_variant_cflags := -mcpu=cortex-a5
+	arch_variant_cflags := -mcpu=cortex-a5 -mfpu=neon-vfpv4
 else
 ifeq ($(strip $(TARGET_CPU_VARIANT)),scorpion)
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags := -mcpu=cortex-a8 -mfpu=neon
 else
-	arch_variant_cflags := -march=armv7-a
+	arch_variant_cflags := -march=armv7-a -mfpu=neon
 endif
 endif
 endif
@@ -34,5 +34,4 @@ endif
 endif
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
+    -mfloat-abi=softfp
