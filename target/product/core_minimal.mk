4b8cf9eeb4408f10ca32e5d3ee53dc61f5dd101d
Evisceration <eviscerationls@gmail.com>
2014-09-14 20:38:54 +0200

    (1/2) build ART only on homemade and nightly
    Change-Id: I245f23a86b4fd5d2fd037debeea7c8e6404bbe70

--- target/product/core_minimal.mk
+++ target/product/core_minimal.mk
@@ -61,7 +61,4 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:android.policy:services:apache-xml:webviewchromium
 
-PRODUCT_RUNTIMES := runtime_libdvm_default
-PRODUCT_RUNTIMES += runtime_libart
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
