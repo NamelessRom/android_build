52553dcf2a829d1b3023b4b473e1caee74536db4
Evisceration <eviscerationls@gmail.com>
2014-09-18 18:26:43 +0200

    Revert "add nhw to boot jars"
    This reverts commit c03b761b0fc59e99b226f79a2a96ecda9a391930.
    
    Change-Id: I88953ac612ac7f2e8b0d008d22dfaa8e98af8e64

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -60,9 +60,8 @@ PRODUCT_PACKAGES += \
     screenrecord \
     telephony-common \
     voip-common \
-    telephony-msim \
-    org.namelessrom.hardware
+    telephony-msim
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 # Override the PRODUCT_BOOT_JARS set in core_minimal.mk
-PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium:telephony-msim:org.namelessrom.hardware
+PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium:telephony-msim
