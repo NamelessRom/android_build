366fc1e69a860f7a344280844705fcb841ee47bf
jumoog <kilianvonpflugk@gmail.com>
2014-09-28 23:04:46 +0200

    move vendor/deps hack into build
    Change-Id: I17b10c83a77202c66611e85026a15c4d0ebef5c3

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -46,7 +46,6 @@ local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
 android_team = "NamelessRom"
 
-
 def check_repo_exists(git_data):
     if not int(git_data.get('total_count', 0)):
         raise Exception("{} not found in {} Github, exiting "
@@ -101,10 +100,11 @@ def parse_device_directory(device_url,device):
 
 
 # Thank you RaYmAn
-def iterate_manifests():
+def iterate_manifests(check_all):
     files = []
-    for file in os.listdir(local_manifest_dir):
-        files.append(os.path.join(local_manifest_dir, file))
+    if check_all:
+        for file in os.listdir(local_manifest_dir):
+            files.append(os.path.join(local_manifest_dir, file))
     files.append('.repo/manifest.xml')
     for file in files:
         try:
@@ -118,11 +118,17 @@ def iterate_manifests():
 
 
 def check_project_exists(url):
-    for project in iterate_manifests():
+    for project in iterate_manifests(True):
         if project.get("name") == url:
             return True
     return False
 
+def check_dup_path(directory):
+    for project in iterate_manifests(False):
+        if project.get("path") == directory:
+            print ("Duplicate path %s found! Removing" % directory)
+            return project.get("name")
+    return None
 
 # Use the indent function from http://stackoverflow.com/a/4590052
 def indent(elem, level=0):
@@ -149,6 +155,12 @@ def create_manifest_project(url, directory,
     if project_exists:
         return None
 
+    dup_path = check_dup_path(directory)
+    if not dup_path is None:
+            write_to_manifest(
+                append_to_manifest(
+                    create_manifest_remove(dup_path)))	
+	
     project = ES.Element("project",
                          attrib={
                              "path": directory,
@@ -158,6 +170,9 @@ def create_manifest_project(url, directory,
                          })
     return project
 
+def create_manifest_remove(url):
+    remove = ES.Element("remove-project", attrib={"name": url})
+    return remove
 
 def append_to_manifest(project):
     try:
@@ -182,12 +197,11 @@ def write_to_manifest(manifest):
 
 
 def parse_device_from_manifest(device):
-    for project in iterate_manifests():
+    for project in iterate_manifests(True):
         name = project.get('name')
         if name.startswith("android_device_") and name.endswith(device):
             return project.get('path')
     return None
-
 
 def parse_device_from_folder(device):
     search = []
@@ -206,8 +220,13 @@ def parse_device_from_folder(device):
     return location
 
 
-def parse_dependency_file(location, fromDeps):
-    dep_file = "nameless.dependencies"
+def parse_dependency_file(location, fromDeps, firstrun):
+    if firstrun: 
+    	dep_file = device + ".dependencies"
+    elif deps_only and not firstrun:
+        dep_file = "cm.dependencies"
+    else:
+        dep_file = "nameless.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
@@ -252,18 +271,18 @@ def create_dependency_manifest(dependencies):
 
 
 def fetch_dependencies(device):
-    location = parse_device_from_folder(device)
+    location = "vendor/nameless/vendorhack/dependencies/"
     if location is None or not os.path.isdir(location):
         raise Exception("ERROR: could not find your device "
                         "folder location, bailing out")
-    dependencies = parse_dependency_file(location, False)
+    dependencies = parse_dependency_file(location, False, True)
     create_dependency_manifest(dependencies)
 
 def fetch_dependencies_via_location(location):
     if location is None or not os.path.isdir(location):
         raise Exception("ERROR: could not find your device "
                         "folder location, bailing out")
-    dependencies = parse_dependency_file(location, True)
+    dependencies = parse_dependency_file(location, True, False)
     if dependencies is not None:
         create_dependency_manifest(dependencies)
 
