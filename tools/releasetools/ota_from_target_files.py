9b01e3c15a566afdeeda24c59e10dea7f00af3fb
Alexander Martinz <eviscerationls@gmail.com>
2015-03-31 21:30:31 +0200

    (1/2) flash supersu when installing rom
    Change-Id: Ia8a550b2e6d04e872d59e18c281f61d5ac09eaec
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -520,6 +520,13 @@ def CopyInstallTools(output_zip):
       install_target = os.path.join("install", os.path.relpath(root, install_path), f)
       output_zip.write(install_source, install_target)
 
+  supersu_path = os.path.join(OPTIONS.input_tmp, "SUPERSU")
+  for root, subdirs, files in os.walk(supersu_path):
+    for f in files:
+      supersu_source = os.path.join(root, f)
+      supersu_target = os.path.join("supersu", os.path.relpath(root, supersu_path), f)
+      output_zip.write(supersu_source, supersu_target)
+
 
 def WriteFullOTAPackage(input_zip, output_zip):
   # TODO: how to determine this?  We don't know what version it will
@@ -759,6 +766,11 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("{*} Formatting user data")
     script.ShowProgress(0.1, 10)
     script.FormatPartition("/data")
+
+  script.Print("{*} Installing SuperSU")
+  script.UnpackPackageDir("supersu", "/tmp/supersu");
+  script.AppendExtra("""run_program("/sbin/busybox", "unzip", "/tmp/supersu/supersu.zip", "META-INF/com/google/android/*", "-d", "/tmp/supersu");""");
+  script.AppendExtra("""run_program("/sbin/busybox", "sh", "/tmp/supersu/META-INF/com/google/android/update-binary", "dummy", "1", "/tmp/supersu/supersu.zip");""");
 
   script.Print("{+} Enjoy Nameless Rom!")
 
